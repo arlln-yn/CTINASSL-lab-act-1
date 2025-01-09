50c52411e7d93211c8de5e80fc324d671a54ff7f
Ma. Darlene A. Arellano <yeyenarellano1@gmail.com>
2025-01-09 23:16:08 +0800

    Remove CORS middleware from auth routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,15 +1,8 @@
 import express from 'express';
 import { registerUser, loginUser, getProfile } from '../controllers/authController.js';
-import cors from 'cors';
+
 
 const router = express.Router();
-
-app.use(
-    cors({
-        origin: 'http://localhost:5173', // Allow requests only from your frontend domain
-        credentials: true, 
-    })
-);
 
 
 router.post('/register', registerUser);
