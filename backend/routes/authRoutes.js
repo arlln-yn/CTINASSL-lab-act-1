28a84e09079994a89fa0224377aa354b4a8eba1e
Ma. Darlene A. Arellano <yeyenarellano1@gmail.com>
2025-01-09 22:42:05 +0800

    removed CORS middleware from authRoutes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,15 +1,9 @@
 import express from 'express';
 import { registerUser, loginUser, getProfile } from '../controllers/authController.js';
-import cors from 'cors';
+
 
 const router = express.Router();
 
-router.use(
-    cors({
-        credentials: true,
-        origin: 'http://localhost:5173'
-    })
-)
 
 
 router.post('/register', registerUser);
